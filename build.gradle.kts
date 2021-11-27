de102acd1e77d156fecd65963b4a03f62f3e2e99
tpepper0408 <45335447+tpepper0408@users.noreply.github.com>
2021-11-27 12:03:52 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.0"
+}
+
+application {
+    mainClass.set("util.Runner")
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.reflections", "reflections", "0.9.12")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.8.1")
+    testImplementation("org.hamcrest", "hamcrest", "2.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
